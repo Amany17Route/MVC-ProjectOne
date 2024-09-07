26c10df3a362974b7033e50d6c521d304a49c6c9
Amany17Route <gg6229663@gmail.com>
2024-09-08 02:18:47 +0300

    Video 04: Action Result (First Controller)

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Project01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //ContentResult result = new ContentResult();
+            //result.Content = "Hello From Content Result";            
+            //return Content("Hello From Content Result") ;
+
+            return Redirect("/Home/AboutUs");
+        }
+
+        
+        public string AboutUs()
+        {
+            return "About Us Action";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,43 +7,55 @@ namespace MVC_Project01
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
             var app = builder.Build();
 
             // app.MapGet("/Home", () => "Hello World!");
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/Home", async context =>
-                {
-                    await context.Response.WriteAsync("Hello From Home");
-                });
+            #region Routing
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/Home", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello From Home");
+            //    });
 
-                endpoints.MapGet("/Products/{ID:int?}", async context =>
-                {
-                    var idData = context.Request.RouteValues["ID"];
+            //    endpoints.MapGet("/Products/{ID:int?}", async context =>
+            //    {
+            //        var idData = context.Request.RouteValues["ID"];
 
-                    if (idData is not null)
-                    {
-                        int data = Convert.ToInt32(idData);
-                        await context.Response.WriteAsync($"Hello From Products  id => {data}");
-                    }
-                    else
-                    {
-                        await context.Response.WriteAsync($"Hello From Products");
-                    }
-                });
+            //        if (idData is not null)
+            //        {
+            //            int data = Convert.ToInt32(idData);
+            //            await context.Response.WriteAsync($"Hello From Products  id => {data}");
+            //        }
+            //        else
+            //        {
+            //            await context.Response.WriteAsync($"Hello From Products");
+            //        }
+            //    });
 
-                endpoints.MapGet("/Books/{ID}/{Author:alpha:minlength(6):maxlength(12)}", async context =>
-                {
-                    int id = Convert.ToInt32(context.Request.RouteValues["ID"]);
-                    string name = context.Request.RouteValues["Author"].ToString();
+            //    endpoints.MapGet("/Books/{ID}/{Author:alpha:minlength(6):maxlength(12)}", async context =>
+            //    {
+            //        int id = Convert.ToInt32(context.Request.RouteValues["ID"]);
+            //        string name = context.Request.RouteValues["Author"].ToString();
 
-                    await context.Response.WriteAsync($"Hello From Books  id=> {id} , At  Author => {name}");
-                });
+            //        await context.Response.WriteAsync($"Hello From Books  id=> {id} , At  Author => {name}");
+            //    });
 
-            });
-  app.Run(async(HttpContext) => { await HttpContext.Response.WriteAsync("Request  Page Is Not Found"); });
+            //}); 
+            #endregion
+
+            // Controller 
+            app.MapControllerRoute(
+                name:"default",
+                pattern:"/{Controller = Home }/{Action = Index}",
+                defaults: new {Controller = "Home" , Action = "Index" }
+                   
+                );
+
+            //app.Run(async(HttpContext) => { await HttpContext.Response.WriteAsync("Request  Page Is Not Found"); });
             app.Run();
 
     
